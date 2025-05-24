6c41aebbdeef01dd0ba853bc1bb3804c775fd6d8
Azat Nasyrov <azat.nasyrov31@gmail.com>
2025-05-24 14:22:48 +0600

    added jwt config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import dbConfig from './config/db.config';
+import jwtConfig from './config/jwt.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [dbConfig],
+      load: [dbConfig, jwtConfig],
       isGlobal: true,
       envFilePath: '.env',
     }),
