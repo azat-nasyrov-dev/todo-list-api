a2b8c50382d8e14b5ae8aa9d9083d4b0e23e16a2
Azat Nasyrov <azat.nasyrov31@gmail.com>
2025-05-24 12:55:12 +0600

    added configuration and connection to the db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import dbConfig from './config/db.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      load: [dbConfig],
       isGlobal: true,
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRootAsync(dbConfig.asProvider()),
   ],
 })
 export class AppModule {}
---
+++ src/config/db.config.ts
@@ -0,0 +1,16 @@
+import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export default registerAs('database', (): TypeOrmModuleOptions => {
+  return {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    synchronize: true,
+    autoLoadEntities: true,
+    logging: true,
+  };
+});
