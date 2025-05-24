f7af5e1721abf91acbfcb9c494ea7a3c8e6b5486
Azat Nasyrov <azat.nasyrov31@gmail.com>
2025-05-24 13:13:39 +0600

    added initial auth module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import dbConfig from './config/db.config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UsersModule } from './users/users.module';
     }),
     TypeOrmModule.forRootAsync(dbConfig.asProvider()),
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
