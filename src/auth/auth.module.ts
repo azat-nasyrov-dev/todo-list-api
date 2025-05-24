a65ad5070529b194d91558ae940cbe1a12f3fe3f
Azat Nasyrov <azat.nasyrov31@gmail.com>
2025-05-24 13:38:09 +0600

    added intermediate abstractions to be independent of the specific implementation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { HashingService } from './hashing/hashing.service';
+import { BcryptService } from './hashing/bcrypt.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: HashingService,
+      useClass: BcryptService,
+    },
+  ],
 })
 export class AuthModule {}
---
+++ src/auth/hashing/hashing.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export abstract class HashingService {
+  public abstract hash(data: string | Buffer): Promise<string>;
+  public abstract compare(data: string | Buffer, encrypted: string): Promise<boolean>;
+}
