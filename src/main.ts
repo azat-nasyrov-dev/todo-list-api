7716fba0d1812367d5db806273131a5d13abc27e
Azat Nasyrov <azat.nasyrov31@gmail.com>
2025-05-24 14:33:30 +0600

    connected the global pipes in the entry point file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }),
+  );
 
   await app.listen(PORT, () => console.log(`App is running on port: ${PORT}`));
 }
