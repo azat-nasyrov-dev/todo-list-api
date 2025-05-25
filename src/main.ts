79b4f29c2011edaf2460dd02341673e8166412eb
Azat Nasyrov <azat.nasyrov31@gmail.com>
2025-05-25 10:53:06 +0600

    added http client files for check requests

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap(): Promise<void> {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  // TODO: Optionally you can add a global prefix
+  /*
   app.setGlobalPrefix('api');
+  */
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }),
